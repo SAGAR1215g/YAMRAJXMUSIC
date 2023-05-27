7322345426b4cd336a73623899f325953865a348
Dil <126651350+SAGAR1215g@users.noreply.github.com>
2023-05-27 07:02:13 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -26,7 +26,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MR_YAMRAJ_XD")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/https://t.me/Sanki_Official")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Sanki_Official")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
